b03c2d5e3347a2678b1dceddf77f1f548def07f4
hoimi <71862402+hoakedo@users.noreply.github.com>
2025-12-17 12:36:21 +0900

    Update run.ps1

--- TimerTrigger/run.ps1
+++ TimerTrigger/run.ps1
@@ -1,307 +1,56 @@
 # Input bindings are passed in via param block.
 param($Timer)
 
-Import-Module Az.Storage -ErrorAction Stop
-$baseUri  = "https://manage.office.com"
-$tenantId = "bc752680-4bf8-4550-a185-fec14c17c3fa"
-
-# ===== 直書き（envは使わない）=====
-#$azstoragestring = "DefaultEndpointsProtocol=https;AccountName=XXXX;AccountKey=YYYY;EndpointSuffix=core.windows.net"
-
-$workspaceId     = "4eace82d-dbcb-4117-9bef-872f18ed14cf"
-$workspaceKey    = "tNHrdV//E0d5RK3myY/xmHMZ1m1K+OQ1oU44rv3evAaC+EwOZ0T2pYD8zd+qkACtxns2kAGIrpkVUGpuFcNdBw=="
-
-$customLogName   = "O365AuditLogs"
-
-$clientID        = "1b997575-614f-4a49-82d3-defa5d3cbf2d"
-$clientSecret    = "NjK8Q~P~M1blB0zXPI.S~kyd6_45NiWPh-sSuaVa"
-$tenantGuid      = "bc752680-4bf8-4550-a185-fec14c17c3fa"
-$domain          = "mocauser38501gmail.onmicrosoft.com"
-
-$contentTypes 	 = "Audit.General,Audit.Exchange,Audit.SharePoint".Split(",")
-$publisher       = "00000000-0000-0000-0000-000000000000"
-$recordTypes     = "0"
-# ===================================
-
-#if ([string]::IsNullOrEmpty($azstoragestring)) {
-#    throw "AzureWebJobsStorage が空です"
-#}
-
-# start / end time を初期化
-$startTime = (Get-Date).AddMinutes(-5).ToString("o")
-$endTime   = (Get-Date).ToString("o")
+#token取得
+$ClientID = "1b997575-614f-4a49-82d3-defa5d3cbf2d"
+$ClientSecret = "NjK8Q~P~M1blB0zXPI.S~kyd6_45NiWPh-sSuaVa"
+$loginURL = "https://login.microsoftonline.com/"
+$tenantdomain = "mocauser38501gmail.onmicrosoft.com"
+$TenantGUID = "bc752680-4bf8-4550-a185-fec14c17c3fa"
+$resource = "https://manage.office.com"
+$body = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret}
+$oauth = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+$headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"} 
 
 
-#Import-module .\TimerTrigger\modules\Write-OMSLogfile.ps1
-###################################################################################
-#  API Log to OMS Log Analytics Workspace
-###################################################################################
-#Credit: https://github.com/tsrob50/LogAnalyticsAPIFunction
-function Write-OMSLogfile {
-    <#
-    .SYNOPSIS
-    Inputs a hashtable, date and workspace type and writes it to a Log Analytics Workspace.
-    .DESCRIPTION
-    Given a  value pair hash table, this function will write the data to an OMS Log Analytics workspace.
-    Certain variables, such as Customer ID and Shared Key are specific to the OMS workspace data is being written to.
-    This function will not write to multiple OMS workspaces.  Build-signature and post-analytics function from Microsoft documentation
-    at https://docs.microsoft.com/azure/log-analytics/log-analytics-data-collector-api
-    .PARAMETER DateTime
-    date and time for the log.  DateTime value
-    .PARAMETER Type
-    Name of the logfile or Log Analytics "Type".  Log Analytics will append _CL at the end of custom logs  String Value
-    .PARAMETER LogData
-    A series of key, value pairs that will be written to the log.  Log file are unstructured but the key should be consistent
-    withing each source.
-    .INPUTS
-    The parameters of data and time, type and logdata.  Logdata is converted to JSON to submit to Log Analytics.
-    .OUTPUTS
-    The Function will return the HTTP status code from the Post method.  Status code 200 indicates the request was received.
-    .NOTES
-    Version:        2.0
-    Author:         Travis Roberts
-    Creation Date:  7/9/2018
-    Purpose/Change: Crating a stand alone function.
-    .EXAMPLE
-    This Example will log data to the "LoggingTest" Log Analytics table
-    $type = 'LoggingTest'
-    $dateTime = Get-Date
-    $data = @{
-        ErrorText   = 'This is a test message'
-        ErrorNumber = 1985
-    }
-    $returnCode = Write-OMSLogfile $dateTime $type $data -Verbose
-    write-output $returnCode
-    #>
-        [cmdletbinding()]
-        Param(
-            [Parameter(Mandatory = $true, Position = 0)]
-            [datetime]$dateTime,
-            [parameter(Mandatory = $true, Position = 1)]
-            [string]$type,
-            [Parameter(Mandatory = $true, Position = 2)]
-            [psobject]$logdata,
-            [Parameter(Mandatory = $true, Position = 3)]
-            [string]$CustomerID,
-            [Parameter(Mandatory = $true, Position = 4)]
-            [string]$SharedKey
-        )
-        Write-Verbose -Message "DateTime: $dateTime"
-        Write-Verbose -Message ('DateTimeKind:' + $dateTime.kind)
-        Write-Verbose -Message "Type: $type"
-        write-Verbose -Message "LogData: $logdata"
+#コンテンツ取得
+$startTime = (Get-Date).ToUniversalTime().AddHours(-12).ToString("o")
+$endTime   = (Get-Date).ToUniversalTime().ToString("o")
+$uri = "https://manage.office.com/api/v1.0/$tenantGuid/activity/feed/subscriptions/content" + "?contentType=Audit.General&startTime=$startTime&endTime=$endTime"
+$uri
+$content = Invoke-RestMethod -Method Get -Headers $headerParams -Uri $uri
+$content
 
-        #region Workspace ID and Key
-        # Workspace ID for the workspace
-        #$CustomerID = 'ENTER WORKSPACE ID HERE'
-
-        # Shared key needs to be set for environment
-        # Below uses an encrypted variable from Azure Automation
-        # Uncomment the next two lines if using Azure Automation Variable and comment the last
-        # $automationVarName = 'Enter Variable Name Here'
-        # $sharedKey = Get-AutomationVariable -name $automationVarName
-        # Key Vault is another secure option for storing the value
-        # Less secure option is to put the key in the code
-        #$SharedKey = 'ENTER WORKSPACE KEY HERE'
-
-        #endregion
-
-        # Supporting Functions
-        # Function to create the auth signature
-        function Build-signature ($CustomerID, $SharedKey, $Date, $ContentLength, $method, $ContentType, $resource) {
-            $xheaders = 'x-ms-date:' + $Date
-            $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
-            $bytesToHash = [text.Encoding]::UTF8.GetBytes($stringToHash)
-            $keyBytes = [Convert]::FromBase64String($SharedKey)
-            $sha256 = New-Object System.Security.Cryptography.HMACSHA256
-            $sha256.key = $keyBytes
-            $calculateHash = $sha256.ComputeHash($bytesToHash)
-            $encodeHash = [convert]::ToBase64String($calculateHash)
-            $authorization = 'SharedKey {0}:{1}' -f $CustomerID,$encodeHash
-            return $authorization
-        }
-        # Function to create and post the request
-        Function Post-LogAnalyticsData ($CustomerID, $SharedKey, $Body, $Type) {
-            $method = "POST"
-            $ContentType = 'application/json'
-            $resource = '/api/logs'
-            $rfc1123date = ($dateTime).ToString('r')
-            $ContentLength = $Body.Length
-            $signature = Build-signature `
-                -customerId $CustomerID `
-                -sharedKey $SharedKey `
-                -date $rfc1123date `
-                -contentLength $ContentLength `
-                -method $method `
-                -contentType $ContentType `
-                -resource $resource
-
-		    # Compatible with previous version
-		    if ([string]::IsNullOrEmpty($LAURI)){
-		    	$LAURI = "https://" + $CustomerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
-		    }
-		    else
-		    {
-		    	$LAURI = $LAURI + $resource + "?api-version=2016-04-01"
-		    }
-		
-            $headers = @{
-                "Authorization" = $signature;
-                "Log-Type" = $type;
-                "x-ms-date" = $rfc1123date
-                "time-generated-field" = $dateTime
-            }
-            $response = Invoke-WebRequest -Uri $LAURI -Method $method -ContentType $ContentType -Headers $headers -Body $body -UseBasicParsing
-            Write-Verbose -message ('Post Function Return Code ' + $response.statuscode)
-            return $response.statuscode
-        }
-
-        # Check if time is UTC, Convert to UTC if not.
-        # $dateTime = (Get-Date)
-        if ($dateTime.kind.tostring() -ne 'Utc'){
-            $dateTime = $dateTime.ToUniversalTime()
-            Write-Verbose -Message $dateTime
-        }
-
-        # Add DateTime to hashtable
-        #$logdata.add("DateTime", $dateTime)
-        $logdata | Add-Member -MemberType NoteProperty -Name "DateTime" -Value $dateTime
-
-        #Build the JSON file
-        $logMessage = ConvertTo-Json $logdata -Depth 20
-        Write-Verbose -Message $logMessage
-
-        #Submit the data
-        $returnCode = Post-LogAnalyticsData -CustomerID $CustomerID -SharedKey $SharedKey -Body ([System.Text.Encoding]::UTF8.GetBytes($logMessage)) -Type $type
-        Write-Verbose -Message "Post Statement Return Code $returnCode"
-        return $returnCode
-    }
-
-function Get-AuthToken{
-    [cmdletbinding()]
-        Param(
-            [Parameter(Mandatory = $true, Position = 0)]
-            [string]$ClientID,
-            [parameter(Mandatory = $true, Position = 1)]
-            [string]$ClientSecret,
-            [Parameter(Mandatory = $true, Position = 2)]
-            [string]$tenantdomain,
-            [Parameter(Mandatory = $true, Position = 3)]
-            [string]$TenantGUID
-        )
-    # Create app of type Web app / API in Azure AD, generate a Client Secret, and update the client id and client secret here
-    if ([string]::IsNullOrEmpty($loginURL)){$loginURL = "https://login.microsoftonline.com"}
-    # Get the tenant GUID from Properties | Directory ID under the Azure Active Directory section
-    
-    $resource = "https://$managementApi"
-    # auth
-    $body = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret}
-    $oauth = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
-    $headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
-    return $headerParams 
+if (-not $content) {
+  Write-Host "No content found in the specified time range."
+  return
 }
 
-function Get-O365Data{
-    [cmdletbinding()]
-    Param(
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string]$startTime,
-        [parameter(Mandatory = $true, Position = 1)]
-        [string]$endTime,
-        [Parameter(Mandatory = $true, Position = 2)]
-        [psobject]$headerParams,
-        [parameter(Mandatory = $true, Position = 3)]
-        [string]$tenantGuid
-    )
+#実データ取得
+$events = @()
+foreach($item in $content){if($null -ne $item.contentUri -and $item.contentUri -ne ""){$data=Invoke-RestMethod -Method Get -Headers $headerParams -Uri $item.contentUri;if($data){if($data -is [System.Array]){$events+=$data}else{$events+=,$data}}}}
+if ($events.Count -eq 0) {Write-Host "No events fetched from contentUri." return}
 
-    #Loop for each content Type like Audit.General
-    foreach ($contentType in $contentTypes) {
-        $listAvailableContentUri = "https://$managementApi/api/v1.0/$tenantGUID/activity/feed/subscriptions/content?contentType=$contentType&PublisherIdentifier=$publisher&startTime=$startTime&endTime=$endTime"
-        do {
-            #List Available Content
-            $contentResult = Invoke-RestMethod -Method GET -Headers $headerParams -Uri $listAvailableContentUri
-            $contentResult.Count
-            #Loop for each Content
-            foreach($obj in $contentResult){
-                #Retrieve Content
-                $data = Invoke-RestMethod -Method GET -Headers $headerParams -Uri ($obj.contentUri)
-                $data.Count
-                #Loop through each Record in the Content
-                foreach($event in $data){
-                    #Filtering for Recrord types
-                    #Get all Record Types
-                    if($recordTypes -eq "0"){
-                        #We dont need Cloud App Security Alerts due to MCAS connector
-                        if(($event.Source) -ne "Cloud App Security"){
-                            #Write each event to Log A
-                            $writeResult  = Write-OMSLogfile (Get-Date) $customLogName $event $workspaceId $workspaceKey
-                            #$writeResult
-                        }
-                    }
-                    else{
-                        #Get only certain record types
-                        $types = $recordTypes.Split(",")
-                        if(($event.RecordType) -in $types){
-                            #We dont need Cloud App Security Alerts due to MCAS connector
-                            if(($event.Source) -ne "Cloud App Security"){
-                                #write each event to Log A
-                                $writeResult  = Write-OMSLogfile (Get-Date) $customLogName $event $workspaceId $workspaceKey
-                                #$writeResult
-                            }
-                        }
-                        
-                    }
-                }
-            }
-            
-            #Handles Pagination
-            $nextPageResult = Invoke-WebRequest -Method GET -Headers $headerParams -Uri $listAvailableContentUri
-            If(($nextPageResult.Headers.NextPageUrl) -ne $null){
-                $nextPage = $true
-                $listAvailableContentUri = $nextPageResult.Headers.NextPageUrl
-            }
-            Else{$nextPage = $false}
-        } until ($nextPage -eq $false)
-    }
-}
-# Get the current universal time in the default string format
-$currentUTCtime = (Get-Date).ToUniversalTime()
-
-# The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
-if ($Timer.IsPastDue) {
-    Write-Host "PowerShell timer is running late!"
+$events | ForEach-Object {
+  $_ | Add-Member -NotePropertyName TimeGenerated -NotePropertyValue (Get-Date).ToUniversalTime().ToString("o") -Force
 }
 
+#送信
+$workspaceId = "4eace82d-dbcb-4117-9bef-872f18ed14cf"
+$workspaceKey = "tNHrdV//E0d5RK3myY/xmHMZ1m1K+OQ1oU44rv3evAaC+EwOZ0T2pYD8zd+qkACtxns2kAGIrpkVUGpuFcNdBw=="
+$logType = "O365Audit"
 
-#add last run time to blob file to ensure no missed packages
-#$endTime = $currentUTCtime | Get-Date -Format yyyy-MM-ddTHH:mm:ss
-#$Context = New-AzStorageContext -ConnectionString $azstoragestring
-#if((Get-AzStorageContainer -Context $Context).Name -contains "lastlog"){
-#    #Set Container
-#    $Blob = Get-AzStorageBlob -Context $Context -Container (Get-AzStorageContainer -Name "lastlog" -Context $Context).Name -Blob "lastlog.log"
-#    $lastlogTime = $blob.ICloudBlob.DownloadText()
-#    $startTime = $lastlogTime | Get-Date -Format yyyy-MM-ddTHH:mm:ss
-#    $endTime | Out-File "$env:TEMP\lastlog.log"
-#    Set-AzStorageBlobContent -file "$env:TEMP\lastlog.log" -Container (Get-AzStorageContainer -Name "lastlog" -Context $Context).Name -Context $Context -Force
-#}
-#else {
-#    #create container
-#    $azStorageContainer = New-AzStorageContainer -Name "lastlog" -Context $Context
-#    $endTime | Out-File "$env:TEMP\lastlog.log"
-#    Set-AzStorageBlobContent -file "$env:TEMP\lastlog.log" -Container $azStorageContainer.name -Context $Context -Force
-#    $startTime = $currentUTCtime.AddSeconds(-300) | Get-Date -Format yyyy-MM-ddTHH:mm:ss
-#}
-#$startTime
-#$endTime
-#$lastlogTime
+$payload = ($events | ConvertTo-Json -Depth 50)
+$bodyBytes = [Text.Encoding]::UTF8.GetBytes($payload)
+$rfc1123   = (Get-Date).ToUniversalTime().ToString("r")
+$stringToSign = "POST`n$($bodyBytes.Length)`napplication/json`nx-ms-date:$rfc1123`n/api/logs"
+$hmac = New-Object System.Security.Cryptography.HMACSHA256
+$hmac.Key = [Convert]::FromBase64String($workspaceKey)
+$signature = [Convert]::ToBase64String(
+  $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($stringToSign))
+)
 
-
-$headerParams = Get-AuthToken $clientID $clientSecret $domain $tenantGuid
-Get-O365Data  $startTime $endTime $headerParams $tenantGuid
-
-
-# Write an information log with the current time.
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+Invoke-RestMethod -Method Post -Uri "https://$workspaceId.ods.opinsights.azure.com/api/logs?api-version=2016-04-01" -Headers @{Authorization="SharedKey ${workspaceId}:${signature}";"Log-Type"=$logType;"x-ms-date"=$rfc1123} -ContentType "application/json" -Body ([Text.Encoding]::UTF8.GetBytes($payload))
 
 
 
